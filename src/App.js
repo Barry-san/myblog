25a6163762e853a34272413fc21e4cfd585736b1
Barry-san <oyeyemimubarak6@gmail.com>
2022-09-24 13:19:51 +0100

    added router and routes to the app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import { auth } from "./config/firebaseConfig";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Navigation/Header";
+import Hero from "./components/Hero";
+import Login from "./components/Login.js";
+import MobileNav from "./components/Navigation/MobileNav";
+import Profile from "./components/Profile";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {window.innerWidth > 450 && <Header></Header>}
+        {window.innerWidth <= 450 && <MobileNav />}
+        <Routes>
+          <Route exact path="/" element={<Hero></Hero>}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          {auth.currentUser && (
+            <Route exact path="/profile" element={<Profile />}></Route>
+          )}
+          <Route
+            path="/*"
+            element={
+              <div>
+                <h1>Error!!!!</h1>
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
