802ec11c8a282aeb4c9a8f3abb53e9c6daba6311
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-04 22:22:51 +0100

    removed comments from file

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// https://firebase.google.com/docs/web/setup#available-libraries
-// TODO: Add SDKs for Firebase products that you want to use
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyApqLOR6u6CemXdzA24GtSdPmzS3tr3VW0",
   authDomain: "react-blog-redux.firebaseapp.com",
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: "1:757951250989:web:d5d7d9e057c2df222faea4",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
