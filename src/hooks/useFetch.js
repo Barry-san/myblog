844d0e7bbf0db51cdbed05aa1419744be575e033
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-08 18:41:59 +0100

    custom hook to fetch blogs

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+import { db } from "../config/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const UseFetch = () => {
+  const [isPending, setPending] = useState(true);
+  const [data, setData] = useState([]);
+  const [Error, setError] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const blogs = await getDocs(collection(db, "blogs"));
+      setData(blogs.docs.map((blog) => ({ ...blog.data(), id: blog.id })));
+      setPending(false);
+    }
+    getData().catch((err) => setError(err));
+  }, [isPending]);
+  return { isPending, data, Error };
+};
+
+export default UseFetch;
