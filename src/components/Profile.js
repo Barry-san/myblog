397d670e2d18215d729dcb7e2b457331b0a40409
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-09 18:18:35 +0100

    removed user from profile for auth

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,13 @@
 import { userContext } from "../App";
-import { useContext } from "react";
+// import { useContext } from "react";
 function Profile() {
-  const user = useContext(userContext);
+  // const user = useContext(userContext);
   return (
     <div className="profile">
       <h1>This is the profile page!</h1>
-      <p>UserName: {user} </p>
+      {/* <p>UserName: {user} </p> */}
       <br />
-      <p>Email: {user}</p>
+      {/* <p>Email: {user}</p> */}
     </div>
   );
 }
