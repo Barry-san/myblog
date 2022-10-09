33897e9153733e213362c65f41929daa6211b753
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-09 18:05:41 +0100

    created basic template for profie

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,13 @@
+import { userContext } from "../App";
+import { useContext } from "react";
 function Profile() {
+  const user = useContext(userContext);
   return (
     <div className="profile">
       <h1>This is the profile page!</h1>
-      <p>UserName: </p>
+      <p>UserName: {user} </p>
       <br />
-      <p>Email: </p>
+      <p>Email: {user}</p>
     </div>
   );
 }
