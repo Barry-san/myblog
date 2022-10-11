838f839ca282bbe4b6cedd9912a6dc70ac96e660
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-11 01:09:41 +0100

    blog details component for viewing blogs

--- src/components/Blogdetails.js
+++ src/components/Blogdetails.js
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+import getData from "../hooks/getData";
+
 const Blogdetails = () => {
-  return <div className="blogdetails"></div>;
+  const { id } = useParams();
+  const details = getData(id);
+  return (
+    <div className="blogdetails">
+      {details.pending && <div className="loading"></div>}
+      {console.log(details)}
+    </div>
+  );
 };
 
 export default Blogdetails;
