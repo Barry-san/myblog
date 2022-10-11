073bc498164198dd3018b4da26232b7f123e1653
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-11 01:09:16 +0100

    made chages to blogcard component

--- src/components/Blogcard.js
+++ src/components/Blogcard.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Blogcard = (prop) => {
   return (
     <div className="blogcard">
-      <p style={{ margin: 0 }}>
-        <strong> {prop.blog.author}</strong>
-      </p>
-      <h2>{prop.blog.title}</h2>
-      <p>{prop.blog.description}</p>
+      <Link to={`/blog/${prop.blog.id}`}>
+        <p style={{ margin: 0 }}>
+          <strong> {prop.blog.author}</strong>
+        </p>
+        <h2>{prop.blog.title}</h2>
+        <p>{prop.blog.description}</p>
+      </Link>
     </div>
   );
 };
