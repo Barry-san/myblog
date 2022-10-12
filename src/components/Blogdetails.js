d4d4accb6a60c58d778ce33fb82086f4b91ba46d
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-12 23:32:12 +0100

    more work on custom hook

--- src/components/Blogdetails.js
+++ src/components/Blogdetails.js
@@ -1,12 +1,17 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import getData from "../hooks/getData";
 
 const Blogdetails = () => {
   const { id } = useParams();
-  const details = getData(id);
+  const [details, setDetails] = useState();
+  useEffect(() => {
+    const blog = getData(id);
+    setDetails(blog.docu);
+  });
   return (
     <div className="blogdetails">
-      {details.pending && <div className="loading"></div>}
+      {/* {details.pending && <div className="loading"></div>} */}
       {console.log(details)}
     </div>
   );
