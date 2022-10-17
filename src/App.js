7d42fe183a118fa518960a51d78df086140c180f
Barry-san <oyeyemimubarak6@gmail.com>
2022-10-17 22:05:05 +0100

    added safety checks to routes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
         </userContext.Provider>
         <Routes>
           <Route exact path="/" element={<Home props={props}></Home>}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
+          {!user && <Route exact path="/login" element={<Login />}></Route>}
           <Route exact path="/register" element={<Register />}></Route>
           <Route
             exact
